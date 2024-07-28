5288bf03742db0ed3dcde3c61b7c4f1c65c05726
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-07-28 21:55:46 +0530

    commit

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,7 +3,73 @@ import"./detail.css";
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="../../../public/avatar.png" alt="" />
+        <h2>Abhijit</h2>
+        <p>Ami ja ka tomar</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="../../../public/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="../../../public/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>privacy & help</span>
+            <img src="../../../public/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="../../../public/arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="../../../public/avatar.png" alt="" />
+              <span>294928401.png</span>
+              </div>
+            
+            <img src="../../../public/download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="../../../public//avatar.png" alt="" />
+              <span>294928401.png</span>
+              </div>
+            
+            <img src="../../../public/download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="../../../public/avatar.png" alt="" />
+              <span>294928401.png</span>
+              </div>
+            
+            <img src="../../../public/download.png" alt="" className="icon" />
+            </div>
+
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="../../../public/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block user</button>
+      </div>
+    </div>
   )
 }
 
