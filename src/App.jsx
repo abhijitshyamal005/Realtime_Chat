13377d98d7699b03c33a17f54979a323c2135259
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-07-30 20:11:50 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import Chat from './components/chat/Chat.jsx';
 import Detail from './components/detail/Detail.jsx';
 import Login from './components/login/Login.jsx';
 import Notification from './components/notification/Notification.jsx';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './lib/Firebase.js';
 
 const App = () => {
 
   const user = false;
+  useEffect(() => {
+    const onSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    })
+    return () => {
+      onSub();
+    }
+  }, []);
 
 
   return (
     <div className='container'>
-      {user?(
+      {user ? (
         <>
-        <List/>
-        <Chat/>
-        <Detail/>
+          <List />
+          <Chat />
+          <Detail />
         </>
-      ):(<Login/>)}
-      <Notification/>
-      
+      ) : (<Login />)}
+      <Notification />
+
     </div>
   )
 }
